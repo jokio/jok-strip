961f6472fe55292b70c02e75d8537205778a5a76
gkardava <qardava.giorgi@gmail.com>
2013-11-03 02:36:46 +0400

    done (need fix repaint)

--- Jok.Strip/Scripts/GameClient.ts
+++ Jok.Strip/Scripts/GameClient.ts
@@ -16,6 +16,12 @@ class GameClient extends ClientEngine.JokClient {
         this.on('disconnect', this.onDisconnect);
         this.on('authorize', this.onAuthorize);
         this.on('msg', this.onMsg);
+        this.on(Game.MessageType.KeyboardOption, this.onKeyboardOptionCome);
+        this.on(Game.MessageType.BadChar, this.onBadChar);
+        this.on(Game.MessageType.RestartState, this.onRestartCall);
+        this.on(Game.MessageType.FirstState, this.onFirstStateCall);
+        this.on(Game.MessageType.State, this.onStateCome);
+        this.on(Game.MessageType.GameEnd, this.onGameEndCall);
     }
 
 
@@ -40,45 +46,68 @@ class GameClient extends ClientEngine.JokClient {
 
     sendChar(char: string) {
 
-        this.sendCommand("msg", { code: Game.Codes.C_UserChar, data: char });
+        this.sendCommand(Game.MessageType.C_UserChar, char);
+    }
+    onKeyboardOptionCome(data: Game.KeyBoardOption) {
+        console.log(data);
+            this.gameState = Game.GameState.Whaiting;
+            //keyboard option
+            this.keyboarOption =data;      
+    }
+    onBadChar(data) {
+        console.log('Dasamatebelia');
+        console.log(data);
     }
 
-    onMsg(msg: Game.IGameToClient) {
-        console.log(msg);
-        if (msg.code == Game.Codes.KeyboardOptionSend) {
-            this.gameState = Game.GameState.Whaiting;
-            
-            //keyboard option
-            this.keyboarOption = <Game.KeyBoardOption>msg.data;
-            return;
-        }
+    
 
-        if (msg.code == Game.Codes.BadChar) {
-           
-            return;
-        }
+    onRestartCall(msg:any) {
+        this.changePlayerState(msg);
+             //clear current state
+            //canvas
+            this.synchronizeCanvasObject();
+            //canvas
+    }
+    changePlayerState(msg:any) {
+        console.log(msg);
+        //todo dasamtavrebelia rom umtkivneulod amoiRos :)
         this.mState = (msg.state[0].userId == <string> window["userid"]) && msg.state ? msg.state[0] : msg.state[1];
         this.fState = (msg.state[0].userId == <string>window["userid"] && msg.state) ? msg.state[1] : msg.state[0];
         if (this.fState) {
             this.fState.time = this.fState.time && this.fState.time > 0 ? Math.floor(this.fState.time / 1000) : Game.GameTable.TIMEOUTTICK / 1000;
         }
         this.mState.time = this.mState.time && this.mState.time > 0 ? Math.floor(this.mState.time / 1000) : Game.GameTable.TIMEOUTTICK / 1000;
-        if (msg.code == Game.Codes.RestartState) {
-            //clear current state
-            //canvas
-            this.synchronizeCanvasObject();
-            //canvas
-        }
-        if (msg.code == Game.Codes.FirstState) {
+    
+    }
+
+    onFirstStateCall(msg: any) {
+        this.changePlayerState(msg);   
             //first run full state
             this.gameState = Game.GameState.Running;
             this.drawAllow = false;
             //'გთხოვთ, დაელოდოთ მეორე მოთამაშეს.'
-            this.drawScreen(Game.Codes.FirstState, this.mState.proverbState, null);
+            this.firstDrawScreen(this.mState.proverbState);
             this.RestartGame();
-        }
+    }
+
+    onStateCome(msg: any) {
+
+        this.changePlayerState(msg)
+
+        if (this.mState.helpkeys)
+            for (var k in this.mState.helpkeys) {
+                //test
+                var element = document.getElementById('btn' + this.mState.helpkeys[k]);
+                element.style.visibility = "hidden";
+                element.style.position = "fixed";
+            }
+        this.drawAllow = true;
+            this.animateWhile();
+    }
+
+    onGameEndCall(msg: any) {
+        this.changePlayerState(msg);
         //დასამატებელია არასწორი არასწორის დამატება.
-        if (this.gameState==Game.GameState.Running && (msg.code == Game.Codes.State || msg.code == Game.Codes.GameEnd)) {
             if (this.mState.helpkeys)
                 for (var k in this.mState.helpkeys) {
                     //test
@@ -86,14 +115,10 @@ class GameClient extends ClientEngine.JokClient {
                     element.style.visibility = "hidden";
                     element.style.position = "fixed";
                 }
-            console.log('0.0.1');
-            this.drawScreen(msg.code, null, null);
-            console.log('0.0.2');
-         
-            if (msg.code == Game.Codes.GameEnd) {
+
                 this.drawAllow = false;
                 this.gameState = Game.GameState.Ended;
-                this.drawScreen(Game.Codes.State, <string>this.mState.proverbState, <string> this.fState.proverbState);
+              //  this.drawScreen(Game.Codes.State, <string>this.mState.proverbState, <string> this.fState.proverbState);
                 //ღილაკების დამალვა.
                 $('#divKeyboard button').each(function (i, element: HTMLElement) {
                     element.style["visibility"] = "hidden";
@@ -109,17 +134,12 @@ class GameClient extends ClientEngine.JokClient {
                 var winner = Game.GameTable.IsWinner(this.mState, this.fState) ? this.mState : this.fState;
                 this.pntext.setText('გამარჯვებულია: ' + winner.userId);
                 this.layer.draw();
-                return;
-            }
-            console.log('0.0.3');
-            console.log(this.timerHendler);
             
-        }
-        if (msg.code == Game.Codes.State) {
-            this.drawAllow = true;
-            this.animateWhile();
-        }
+    }
 
+    onMsg(msg: Game.IGameToClient) {
+
+        console.log("aqar unda Semosuliyo");
     }
 
     timerTick() {
@@ -128,7 +148,7 @@ class GameClient extends ClientEngine.JokClient {
                 this.mState.time = Game.GameTable.TIMEOUTTICK / 1000;
             if (this.fState.time <= 0)
                 this.fState.time = Game.GameTable.TIMEOUTTICK / 1000;
-            this.drawScreen(null, null, null);
+            this.drawScreen();
             this.mState.time--;
             this.fState.time--;
         
@@ -154,10 +174,7 @@ class GameClient extends ClientEngine.JokClient {
         this.layer.removeChildren();
         this.chars = [];
         this.rects = [];
-
-
-       
-        this.drawScreen(Game.Codes.FirstState, this.mState.proverbState, null);
+        this.firstDrawScreen(this.mState.proverbState);
         this.drawAllow = true;
         this.layer.draw();
     }
@@ -178,12 +195,12 @@ class GameClient extends ClientEngine.JokClient {
             if (Game.GameState.Ended== this.gameState) {
                 this.layer.clear();
                 console.log('4.3');
-                this.sendCommand("msg", { code: Game.Codes.C_RestartRequest });
+                this.sendCommand(Game.MessageType.C_RestartRequest ,"{RESTART}");
             } else {
                 console.log('4.5');
                 this.pntext.setText('გთხოვთ დაელოდოთ მეორე მოთამაშეს!');
                 this.layer.draw();
-                this.sendCommand('msg', { code: Game.Codes.C_FirstGameStart });
+                this.sendCommand(Game.MessageType.C_FirstGameStart ,"{FirstState}");
             }
         }
     }
@@ -193,49 +210,19 @@ class GameClient extends ClientEngine.JokClient {
         if (this.drawAllow) {
             this.timerTick();
             this.timerHendler = setTimeout(()=>this.animateWhile(), 1000);
-        }}
-
-    //--CANVAS
-    stage: Kinetic.Stage;
-    layer: Kinetic.Layer;
-    rects: Kinetic.Rect[] = [];
-    chars: Kinetic.Text[] = [];
-    pntext: Kinetic.Text;
-    drawScreen(code?: Game.Codes, text1?: string, text2?: string) {
-
-        if (code == null) {
-            console.log('1.0');
-            if (!this.drawAllow)
-                return;
-             console.log(1.1);
-            if (!(this.gameState==Game.GameState.Running&& this.mState && this.fState && this.fState.proverbState && this.mState.proverbState)) {
-                console.log('test. Ar unda Semovides. 1.1.2');
-                return;
-            }
-            console.log(1.2);
-           // this.layer.clear();
-            var chars = this.mState.proverbState.split('');
-            console.log(1.3);
-            for (var i = 0; i < chars.length; i++) {
-                    if (this.fState.proverbState.charAt(i) != Game.GameTable.XCHAR && this.mState.proverbState.charAt(i) != this.fState.proverbState.charAt(i)) {
-                        this.rects[i].setStroke('#21A527');
-                    }
-                this.chars[i].setText(chars[i]);
-            }
-            //todo: optimizacia max incoreqtze Sesazlebelia!
-            console.log('1.4.3');
-            //RENDER
-          
-            this.pntext.setText('თქვენ სიცოცხლე: ' + GameClient.getPercent(this.mState.incorect / this.mState.maxIncorrect) + '%    დარჩენილი დრო: ' + this.mState.time + '\r\n' + ' მოწინააღმდეგე სიცოცხლე: ' + GameClient.getPercent(this.fState.incorect / this.fState.maxIncorrect) + ' %    დარჩენილი დრო: ' + this.fState.time)
-            console.log('1.4.4');
-             this.layer.draw();
-            console.log(1.4);
-            return;
         }
+    }
+
+    gameEndDrawScreen(meText: string, frText: string) {
+        this.drawAllow = true;
+        this.drawScreen();
+        this.drawAllow = false;
+    }
+    firstDrawScreen(text:string) {
 
         var maxWidth: number = <number>this.layer.getAttr('width');
         ////--clear
-        if (code == Game.Codes.FirstState && this.chars.length<2) {
+        if (this.chars.length < 2) {
             console.log('2');
             //
             //clear ALL
@@ -246,7 +233,7 @@ class GameClient extends ClientEngine.JokClient {
             var rectWidth = 40;
             var rectheight = 40;
             //loadorFullUpdate
-            var chararr = text1.split('');
+            var chararr = text.split('');
             for (var i = 0; i < chararr.length; i++) {
                 var rect = new Kinetic.Rect({
                     x: x + q,
@@ -295,17 +282,45 @@ class GameClient extends ClientEngine.JokClient {
                 fill: 'black'
             });
             this.layer.add(this.pntext);
-        //    this.layer.draw(); ar daixatos pirvelad Sesvlisas
+            //    this.layer.draw(); ar daixatos pirvelad Sesvlisas
             return;
         }
-        if (code == Game.Codes.GameEnd) {
-            //dasasruli // mogebuli unda vaCveno
-           
-            this.drawScreen(null, null, null);
-            //გამოვიტანოთ ვინ გაიმარჯვა
-
-            //გამოვითანოთ ხელახლა თამაშის ღილაკი
-        }
+    }
+    //--CANVAS
+    stage: Kinetic.Stage;
+    layer: Kinetic.Layer;
+    rects: Kinetic.Rect[] = [];
+    chars: Kinetic.Text[] = [];
+    pntext: Kinetic.Text;
+    drawScreen() {
+      
+            console.log('1.0');
+            if (!this.drawAllow)
+                return;
+             console.log(1.1);
+            if (!(this.gameState==Game.GameState.Running&& this.mState && this.fState && this.fState.proverbState && this.mState.proverbState)) {
+                console.log('test. Ar unda Semovides. 1.1.2');
+                return;
+            }
+            console.log(1.2);
+            // this.layer.clear();
+            var chars = this.mState.proverbState.split('');
+            console.log(1.3);
+            for (var i = 0; i < chars.length; i++) {
+                    if (this.fState.proverbState.charAt(i) != Game.GameTable.XCHAR && this.mState.proverbState.charAt(i) != this.fState.proverbState.charAt(i)) {
+                        this.rects[i].setStroke('#21A527');
+                    }
+                this.chars[i].setText(chars[i]);
+            }
+            //todo: optimizacia max incoreqtze Sesazlebelia!
+            console.log('1.4.3');
+            //RENDER
+          
+            this.pntext.setText('თქვენ სიცოცხლე: ' + GameClient.getPercent(this.mState.incorect / this.mState.maxIncorrect) + '%    დარჩენილი დრო: ' + this.mState.time + '\r\n' + ' მოწინააღმდეგე სიცოცხლე: ' + GameClient.getPercent(this.fState.incorect / this.fState.maxIncorrect) + ' %    დარჩენილი დრო: ' + this.fState.time)
+            console.log('1.4.4');
+             this.layer.draw();
+            console.log(1.4);
+       
     }
 
 
