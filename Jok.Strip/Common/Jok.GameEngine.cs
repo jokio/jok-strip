55dc4c0b9981887515dd54deb3b7e37b21516e51
Ezeki Zibzibadze <ez@jok.io>
2013-11-06 22:44:43 +0400

    added timer

--- Jok.Strip/Common/Jok.GameEngine.cs
+++ Jok.Strip/Common/Jok.GameEngine.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Runtime.Serialization;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Web;
 
 namespace Jok.Strip.Common
@@ -556,5 +557,69 @@ namespace Jok.Strip.Common
         public string IPAddress { get; set; }
         public bool IsOnline { get; set; }
         public int Points { get; set; }
+    }
+
+    class JokTimerInternal<T> : IJokTimer<T>
+    {
+        Timer timer;
+
+        public void SetInterval(Action<T> method, T state, int delayInMilliseconds)
+        {
+            Stop();
+
+            timer = new Timer(delayInMilliseconds);
+            timer.Elapsed += (source, e) =>
+            {
+                method(state);
+            };
+
+            timer.Enabled = true;
+            timer.Start();
+        }
+
+        public void SetTimeout(Action<T> method, T state, int delayInMilliseconds)
+        {
+            Stop();
+
+            timer = new System.Timers.Timer(delayInMilliseconds);
+            timer.Elapsed += (source, e) =>
+            {
+                method(state);
+            };
+
+            timer.AutoReset = false;
+            timer.Enabled = true;
+            timer.Start();
+        }
+
+        public void Stop()
+        {
+            try
+            {
+                if (timer != null)
+                {
+                    timer.Stop();
+                    timer.Close();
+                    timer.Dispose();
+                    timer = null;
+                }
+            }
+            catch { }
+        }
+    }
+
+    public static class JokTimer<T>
+    {
+        public static IJokTimer<T> Create()
+        {
+            return new JokTimerInternal<T>();
+        }
+    }
+
+    public interface IJokTimer<T>
+    {
+        void SetInterval(Action<T> method, T state, int delayInMilliseconds);
+        void SetTimeout(Action<T> method, T state, int delayInMilliseconds);
+        void Stop();
     }
 }
