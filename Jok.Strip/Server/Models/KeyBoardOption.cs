c8a8345a712573d064c11cff2ef489a5cfb2b9f2
gkardava <qardava.giorgi@gmail.com>
2013-11-10 23:22:02 +0400

    keyboard option added to source control

---
+++ Jok.Strip/Server/Models/KeyBoardOption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Jok.Strip.Server.Models
+{
+    [DataContract]
+    public struct KeyboardOption
+    {
+        [DataMember]
+        public int From { set; get; }
+        [DataMember]
+        public int To { set; get; }
+    }
+}
