db82c5e69984ddf008c081eaadafa413d323232d
Ezeki Zibzibadze <ez@jok.io>
2013-11-23 03:54:23 +0400

    added authentication check on controller

--- Jok.Strip/Controllers/HomeController.cs
+++ Jok.Strip/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Jok.GameEngine;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,9 +15,39 @@ namespace Jok.Strip.Controllers
             return RedirectToAction("Play");
         }
 
-        public ActionResult Play(string channel, int userid = 32)
+        public ActionResult Play(string id, string sid)
         {
-            ViewBag.UserID = userid;
+            var loginUrl = ConfigurationManager.AppSettings["jok:SiteUrl"] + "/joinus?returnUrl=" + Request.Url;
+            var exitUrl = ConfigurationManager.AppSettings["jok:SiteUrl"] + "/lobby/japmatrix";
+
+            if (!String.IsNullOrEmpty(sid))
+            {
+                var cookie = Request.Cookies["sid"];
+                if (cookie == null)
+                    cookie = new HttpCookie("sid", sid);
+
+                cookie.Value = sid;
+                cookie.Expires = DateTime.UtcNow.AddYears(30);
+
+                if (Request.Url.Host.Contains('.'))
+                    cookie.Domain = Request.Url.Host.Substring(Request.Url.Host.IndexOf('.'));
+
+                Response.Cookies.Add(cookie);
+            }
+            else
+            {
+                sid = Request.Cookies["sid"] == null ? "" : Request.Cookies["sid"].Value;
+            }
+
+            var userInfo = JokAPI.GetUser(sid, Request.UserHostAddress);
+            if (userInfo == null || !userInfo.IsSuccess)
+                return Redirect(loginUrl);
+
+
+            ViewBag.SID = sid;
+            ViewBag.Channel = id;
+            ViewBag.AuthorizationUrl = loginUrl;
+            ViewBag.ExitUrl = exitUrl;
 
             return View();
         }
